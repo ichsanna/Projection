0c62f0223128d2444134dc4845441ee9005fd460
ichsanna <43778566+ichsanna@users.noreply.github.com>
2019-05-12 22:01:39 +0700

    Updatee
    +Resized Window
    +Persiapan transformasi

--- Projection/MainWindow.xaml.cs
+++ Projection/MainWindow.xaml.cs
@@ -17,17 +17,54 @@ using System.Windows.Media.Media3D;
 
 namespace Projection
 {
+    // Kelompok:
+    // Dian Rahmaji (17/413896/TK/46336)
+    // Raden Ichsan Nur Aldiansyah (17/413915/TK/46355)
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
         Model3DGroup objects = new Model3DGroup();
+        Model3DGroup sumbu = new Model3DGroup();
+        double[,] matriks = new double[,]
+        {
+            {0,0,0,0},
+            {0,0,0,0},
+            {0,0,0,0},
+            {0,0,0,0}
+        };
+        Point3D[] titik = new Point3D[10] {
+                new Point3D(0,0,0),
+                new Point3D(0,0,0),
+                new Point3D(0,0,0),
+                new Point3D(0,0,0),
+                new Point3D(0,0,0),
+                new Point3D(0,0,0),
+                new Point3D(0,0,0),
+                new Point3D(0,0,0),
+                new Point3D(0,0,0),
+                new Point3D(0,0,0)
+        };
         public MainWindow()
         {
             InitializeComponent();
             gambarsumbu();
+            koordinatrumah();
             gambarrumah();
+        }
+        private void koordinatrumah()
+        {
+            titik[0] = new Point3D(0, 0, 0);
+            titik[1] = new Point3D(5, 0, 0);
+            titik[2] = new Point3D(5, 5, 0);
+            titik[3] = new Point3D(0, 5, 0);
+            titik[4] = new Point3D(0, 0, 3);
+            titik[5] = new Point3D(5, 0, 3);
+            titik[6] = new Point3D(5, 5, 3);
+            titik[7] = new Point3D(0, 5, 3);
+            titik[8] = new Point3D(2.5, 0, 5);
+            titik[9] = new Point3D(2.5, 5, 5);
         }
         private void gambarsumbu()
         {
@@ -37,47 +74,50 @@ namespace Projection
             sumbux.AddPipe(new Point3D(-100, 0, 0), new Point3D(100, 0, 0), 0, 0.1, 90);
             sumbuy.AddPipe(new Point3D(0, -100, 0), new Point3D(0, 100, 0), 0, 0.1, 90);
             sumbuz.AddPipe(new Point3D(0, 0, -100), new Point3D(0, 0, 100), 0, 0.1, 90);
-            objects.Children.Add(new GeometryModel3D
+            sumbu.Children.Add(new GeometryModel3D
             {
                 Geometry = sumbux.ToMesh(true),
                 Material = MaterialHelper.CreateMaterial(Colors.DarkRed)
             });
-            objects.Children.Add(new GeometryModel3D
+            sumbu.Children.Add(new GeometryModel3D
             {
                 Geometry = sumbuy.ToMesh(true),
                 Material = MaterialHelper.CreateMaterial(Colors.ForestGreen)
             });
-            objects.Children.Add(new GeometryModel3D
+            sumbu.Children.Add(new GeometryModel3D
             {
                 Geometry = sumbuz.ToMesh(true),
                 Material = MaterialHelper.CreateMaterial(Colors.RoyalBlue)
             });
-            placedobjects.Content = objects;
+            placedobjects.Content = sumbu;
         }
         private void gambarrumah()
         {
             var kerangkarumah = new MeshBuilder(false, false);
-            kerangkarumah.AddPipe(new Point3D(0, 0, 0), new Point3D(5, 0, 0), 0, 0.5, 90);
-            kerangkarumah.AddPipe(new Point3D(5, 0, 0), new Point3D(5, 5, 0), 0, 0.5, 90);
-            kerangkarumah.AddPipe(new Point3D(5, 5, 0), new Point3D(0, 5, 0), 0, 0.5, 90);
-            kerangkarumah.AddPipe(new Point3D(0, 5, 0), new Point3D(0, 0, 0), 0, 0.5, 90);
-            kerangkarumah.AddPipe(new Point3D(0, 0, 3), new Point3D(5, 0, 3), 0, 0.5, 90);
-            kerangkarumah.AddPipe(new Point3D(5, 0, 3), new Point3D(5, 5, 3), 0, 0.5, 90);
-            kerangkarumah.AddPipe(new Point3D(5, 5, 3), new Point3D(0, 5, 3), 0, 0.5, 90);
-            kerangkarumah.AddPipe(new Point3D(0, 5, 3), new Point3D(0, 0, 3), 0, 0.5, 90);
-            kerangkarumah.AddPipe(new Point3D(0, 0, 0), new Point3D(0, 0, 3), 0, 0.5, 90);
-            kerangkarumah.AddPipe(new Point3D(5, 0, 0), new Point3D(5, 0, 3), 0, 0.5, 90);
-            kerangkarumah.AddPipe(new Point3D(0, 5, 0), new Point3D(0, 5, 3), 0, 0.5, 90);
-            kerangkarumah.AddPipe(new Point3D(5, 5, 0), new Point3D(5, 5, 3), 0, 0.5, 90);
-            kerangkarumah.AddPipe(new Point3D(0, 0, 3), new Point3D(2.5, 2.5, 5), 0, 0.5, 90);
-            kerangkarumah.AddPipe(new Point3D(5, 0, 3), new Point3D(2.5, 2.5, 5), 0, 0.5, 90);
-            kerangkarumah.AddPipe(new Point3D(5, 5, 3), new Point3D(2.5, 2.5, 5), 0, 0.5, 90);
-            kerangkarumah.AddPipe(new Point3D(0, 5, 3), new Point3D(2.5, 2.5, 5), 0, 0.5, 90);
+            kerangkarumah.AddPipe(titik[0], titik[1], 0, 0.2, 90);
+            kerangkarumah.AddPipe(titik[1], titik[2], 0, 0.2, 90);
+            kerangkarumah.AddPipe(titik[2], titik[3], 0, 0.2, 90);
+            kerangkarumah.AddPipe(titik[3], titik[0], 0, 0.2, 90);
+            kerangkarumah.AddPipe(titik[4], titik[5], 0, 0.2, 90);
+            kerangkarumah.AddPipe(titik[5], titik[6], 0, 0.2, 90);
+            kerangkarumah.AddPipe(titik[6], titik[7], 0, 0.2, 90);
+            kerangkarumah.AddPipe(titik[7], titik[4], 0, 0.2, 90);
+            kerangkarumah.AddPipe(titik[0], titik[4], 0, 0.2, 90);
+            kerangkarumah.AddPipe(titik[1], titik[5], 0, 0.2, 90);
+            kerangkarumah.AddPipe(titik[2], titik[6], 0, 0.2, 90);
+            kerangkarumah.AddPipe(titik[3], titik[7], 0, 0.2, 90);
+            kerangkarumah.AddPipe(titik[4], titik[8], 0, 0.2, 90);
+            kerangkarumah.AddPipe(titik[5], titik[8], 0, 0.2, 90);
+            kerangkarumah.AddPipe(titik[6], titik[9], 0, 0.2, 90);
+            kerangkarumah.AddPipe(titik[7], titik[9], 0, 0.2, 90);
+            kerangkarumah.AddPipe(titik[9], titik[8], 0, 0.2, 90);
             objects.Children.Add(new GeometryModel3D
             {
                 Geometry = kerangkarumah.ToMesh(true),
                 Material = MaterialHelper.CreateMaterial(Colors.DarkBlue)
             });
+            placedobjects.Children.Clear();
+            objects.Children.Add(sumbu);
             placedobjects.Content = objects;
         }
     }
